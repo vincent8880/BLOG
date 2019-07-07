6997b69cb3e066b88f62253b89bb687034136254
vincent <ododovincent54@gmail.com>
2019-07-07 22:26:10 +0300

    updated my index.html file

--- app/main/views.py
+++ app/main/views.py
@@ -1,103 +1,48 @@
-from flask import render_template, redirect, url_for, flash
-from flask_login import login_required, current_user
-from flask_admin.contrib.sqla import ModelView
-from . forms import PostForm, CommentForm, SubscribersForm
-from .import main
-from .. import db, basic_auth
-import markdown2
-from .email import mail_message
-from ..models import User, Post, Role, Comment, Subscribers
-from datetime import datetime
+{% extends 'base.html' %}
+{% import 'bootstrap/wtf.html' as wtf %}
 
-@main.route('/',methods=['GET', 'POST'])
-def index():
-    title= "Blog On | Home "
-    all = Post.query.order_by('id').all()
-    print(f'blogs {all}')
-  
-    subscribers = SubscribersForm()
-    try:
-        if subscribers.validate_on_submit():
-            subscriber = Subscribers(email = subscribers.email.data)
-            db.session.add(subscriber)
-            db.session.commit()
-            flash('You are now subscribed!')
-            mail_message("Welcome to Blog On","email/welcome",subscriber.email,subscriber=subscriber)
-            print("sent")
-            return redirect(url_for('main.index'))
-    except:
-        return redirect(url_for('main.index'))
-        
-        
-    return render_template('index.html', title = title, posts=all, subscribers=subscribers)
-    
+{% block content %}
+<div class="header">
+    <div class="intro">
+        <h1 class="display-4">Hey There {{current_user.username}}!..</h1>
+        {% if current_user.is_admin %}
+        <p>Welcome to Blog On!... You can now start posting your articles here!...</p> 
+        <button><a href="{{url_for('main.post')}}">Post article</a> </button> 
+        {% else %}
+        <p>Welcome! You can now Read and share articles!..</p>
+        <div class="subscribers">
+        {{wtf.quick_form(subscribers)}}
+        {% for message in get_flashed_messages() %}
+            <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            {{ message }}
+            </div>
+        {% endfor %}
+        </div>
+        {% endif %}
+    </div>
+</div>
 
-
-@main.route('/profile/<username>')
-@login_required
-
-def profile(username):
-    
-    user = User.query.filter_by(username = username).first_or_404()
-    title = "Profile"       
-    return render_template('profile.html' , user=user,title=title)
-
-@main.route('/post', methods=['GET', 'POST'])
-def post():
-    all = Post.query.all()
-    all.reverse()
-    print(all)
-
-    Comments = CommentForm()
-    if Comments.validate_on_submit():
-        comment = Comment(comment = Comments.comment.data, commenter = Comments.commenter.data)
-        db.session.add(comment)
-        db.session.commit()
-        print(comment)
-        return redirect(url_for('main.post'))
-
-    allcomments = Comment.query.all()
-    title = "Post Article"
-    Blog = PostForm()
-    # try:
-    if Blog.validate_on_submit():
-        post = Post( title = Blog.title.data ,post = Blog.Entry.data, user_id = current_user.id, timeposted = datetime.utcnow() )
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('main.post'))
-    # except:
-    #     flash("Sorry you can NOT post more than 225 characters for now. We are aware of this situation and are currently working on this.")
-    #     return redirect(url_for('main.post'))
-
-    return render_template('post.html', Post = Blog, title = title, posts = all, comment = Comments, allcomments = allcomments)
-
-
-
-@main.route('/post/<id>', methods=['POST','GET'])
-def fullpost(id):
-    
-    title= f'Posts' 
-    post = Post.query.filter_by(id=id).first()
-    Comments = CommentForm()
-    if Comments.validate_on_submit():
-        comment = Comment(comment = Comments.comment.data, post_id=id, commenter = Comments.commenter.data)
-        db.session.add(comment)
-        db.session.commit()
-        print(comment)
-        return redirect(url_for('main.fullpost', id=post.id))
-    allcomments = Comment.query.all()
-    postcomments = Comment.query.filter_by(post_id=id).all()
-     
-
-    return render_template('fullpost.html', title = title, post = post, comment = Comments, allcomments = allcomments, postcomments = postcomments)
-
-            
-
-@main.route('/<int:id>/delete', methods=['POST'])
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?, (id,)')
-    db.commit()
-    return redirect(url_for('main.index'))
+<div class="container-fluid">
+    <div class="row posts">
+        {% for post in posts %}
+        <div class="col-md-5 article">
+            <div class="wrap">
+                <h4>
+                    <a href="{{url_for('main.fullpost',id=post.id)}}">{{post.title}}</a>
+                </h4>
+                <hr>
+                <br>
+                <i><sub>{{post.timeposted}}</sub></i>
+                <br>
+                {{post.post | truncate(250)}}
+                <br>
+            <button>
+                <a href="{{url_for('main.fullpost',id=post.id)}}">Read article</a>
+            </button>
+            </div> 
+        </div>
+        {% endfor %}
+    </div>
+</div>
+{% endblock %}
